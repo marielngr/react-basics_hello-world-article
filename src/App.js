21caf02ff65c08700824b1a8b3511f897145b55c
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-13 11:30:14 +0100

    solve

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Platzhalter</h1>
+      <p>Lass dich überraschen!</p>
+    </article>
+  );
 }
